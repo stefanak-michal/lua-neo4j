24f61c8bee8ad72ce6aa269a6318bdf9cfe5b768
Michal Stefanak <stefanak.michal@gmail.com>
2022-02-17 18:40:20 +0100

    packer.. wip

--- src/connection.lua
+++ src/connection.lua
@@ -14,38 +14,38 @@ function packVersions()
   while #connection.versions < 4 do
     table.insert(connection.versions, 0)
   end
-  
+
   for i, v in ipairs(connection.versions) do
     v = string.gsub(tostring(v), '%.', '')
     v = string.reverse(tostring(v))
     v = string.format('%04d', v)
     for r in string.gmatch(v, '[0-9]') do
-      table.insert(output, string.pack('B', r))
+      table.insert(output, string.pack('>B', r))
     end
   end
-  
+
   return table.concat(output, '')
 end
 
 function unpackVersion(msg)
   local output = {}
   for b in string.gmatch(msg, '.') do
-    b, _ = string.unpack('B', b)
+    b, _ = string.unpack('>B', b)
     table.insert(output, b)
   end
-  
+
   while output[1] == 0 do
     table.remove(output, 1)
   end
-  
+
   return string.reverse(table.concat(output, '.'))
 end
 
 function connection.connect()
   client = nil
-  
+
   local conn, err, result, sended, msg
-  
+
   conn, err = socket.tcp()
   if conn == nil then
     return conn, err
@@ -57,10 +57,10 @@ function connection.connect()
   if result == nil then
     return result, err
   end
-  
+
   conn:settimeout(1)
   conn:setoption('keepalive', true)
-  
+
   -- Handshake
   err = connection.write(string.char(0x60, 0x60, 0xB0, 0x17))
   if err ~= nil then
@@ -72,7 +72,7 @@ function connection.connect()
   if err ~= nil then
     return nil, err
   end
-  
+
   -- Read version
   msg, err = connection.read(4)
   if msg == nil then
@@ -80,7 +80,7 @@ function connection.connect()
   end
 
   local version = unpackVersion(msg)
-    
+
   if #version > 0 then
     client = conn
     return version
@@ -93,10 +93,10 @@ function connection.write(data)
   if client == nil or client:getpeername() == nil then
     return 'Not connected'
   end
-  
+
   local sended, err = client:send(data)
   -- todo chunking
-  
+
   return nil
 end
 
@@ -104,7 +104,7 @@ function connection.read(length)
   if client == nil or client:getpeername() == nil then
     return nil, 'Not connected'
   end
-  
+
   return client:receive(length)
   -- todo chunking
 end
--- src/packer.lua
+++ src/packer.lua
@@ -1,6 +1,144 @@
 local packer = {}
 
-function packer.pack()
+local SMALL, MEDIUM, LARGE, HUGE = 16, 256, 65536, 4294967295
+
+function packer.pack(signature, ...)
+  local params = {...}
+  local output = ''
+
+  if #params < SMALL then
+    output = output .. string.pack('>B', 0xB0 | #params)
+  elseif #params < MEDIUM then
+    output = output .. string.char(0xDC) .. pack('>B', #params)
+  elseif #params < LARGE then
+    output = output .. string.char(0xDD) .. pack('>H', #params)
+  else
+    return nil, 'Too many parameters'
+  end
+
+  output = output .. string.char(signature)
+
+  for i, v in ipairs(params) do
+    output = output .. p(v)
+  end
+
+  return output
+end
+
+function p(param)
+  if type(param) == 'number' then
+    if math.type(param) == 'integer' then
+      return packInteger(param)
+    elseif math.type(param) == 'float' then
+      return packFloat(param)
+    else
+      --error
+    end
+  elseif type(param) == 'string' then
+    return packString(param)
+  elseif type(param) == 'table' then
+    if param.type == 'list' then
+      return packList(param)
+    elseif param.type == 'dictionary' then
+      return packDictionary(param)
+    elseif param.type == 'structure' then
+      return packStructure(param)
+    end
+  elseif type(param) == 'boolean' and param == true then
+    return string.char(0xC3)
+  elseif type(param) == 'boolean' and param == false then
+    return string.char(0xC2)
+  elseif param == nil then
+    return string.char(0xC0)
+  else
+    --error
+  end
+end
+
+function packInteger(param)
+  if param >= 0 and param <= 127 then
+    return string.pack('>B', 0 | param)
+  elseif param >= -16 and param < 0 then
+    return string.pack('>b', 0xF0 | param)
+  elseif param >= -128 and param <= -17 then
+    return string.char(0xC8) .. string.pack('>b', param)
+  elseif ((param >= 128 and param <= 32767) or (param >= -32768 and param <= -129)) then
+    return string.char(0xC9) .. string.pack('>h', param)
+  elseif ((param >= 32768 and param <= 2147483647) or (param >= -2147483648 and param <= -32769)) then
+    return string.char(0xCA) .. string.pack('>l', param)
+  elseif ((param >= 2147483648 and param <= 9223372036854775807) or (param >= -9223372036854775808 and param <= -2147483649)) then
+    return string.char(0xCB) .. string.pack('>i64', param)
+  else
+    --error
+  end
+end
+
+function packFloat(param)
+  return string.char(0xC1) .. string.pack('>d', param)
+end
+
+function packString(param)
+  if #param < SMALL then
+    return pack('B', 0x80 | #param) .. param
+  elseif #param < MEDIUM then
+    return string.char(0xD0) .. string.pack('>B', #param) .. param
+  elseif #param < LARGE then
+    return string.char(0xD1) .. string.pack('>H', #param) .. param
+  elseif #param < HUGE then
+    return string.char(0xD2) .. string.pack('>L', #param) . param
+  else
+    --error
+  end
+end
+
+function packList(param)
+  param.type = nil
+  local output = ''
+
+  if #param < SMALL then
+    output = output .. string.pack('>B', 0x90 | #param)
+  elseif #param < MEDIUM then
+    output = output .. string.char(0xD4) .. string.pack('>B', #param)
+  elseif #param < LARGE then
+    output = output .. string.char(0xD5) .. string.pack('>H', #param)
+  elseif #param < HUGE then
+    output = output .. string.char(0xD6) .. string.pack('>L', #param)
+  else
+    --error
+  end
+
+  for i, v in ipairs(param) do
+    output = output .. p(v)
+  end
+
+  return output
+end
+
+function packDictionary(param)
+  param.type = nil
+  local output = ''
+
+  if #param < SMALL then
+    output = output .. pack('>B', 0xA0 | #param)
+  elseif #param < MEDIUM then
+    output = output .. string.char(0xD8) .. string.pack('>B', #param)
+  elseif #param < LARGE then
+    output = output .. string.char(0xD9) .. string.pack('>H', #param)
+  elseif #param < HUGE then
+    output = output .. string.char(0xDA) .. string.pack('>L', #param)
+  else
+    --error
+  end
+
+  for k, v in pairs(param) do
+    output = output .. p(tostring(k)) .. p(v)
+  end
+
+  return output
+end
+
+function packStructure(param)
+  --todo
 end
 
 return packer
